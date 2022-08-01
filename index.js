6d2883c120bc6671c7516a63d208399cc6b49f12
jaykobpc <watenga.jacob@gmail.com>
2022-08-02 00:54:36 +0300

    fixes

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ fetch("data.json")
     json.forEach((item) => {
       const result = `
         <div data-bar="${item.amount}%" class="graphbox">
-            <span id="tooltip" class="graphbox__tooltip" role="tooltip">${item.amount}%</span>
-            <span id="graphbar" style="height: ${item.amount}%" class="graphbox__bar ${item.amount > 50 ? 'color-cyan' : ''}"></span>
+            <span class="graphbox__tooltip" role="tooltip">${item.amount}%</span>
+            <span style="height: ${item.amount}%" class="graphbox__bar ${item.amount > 50 ? 'color-cyan' : ''}"></span>
             <span class="graphbox__text">${item.day}</span>
         </div>
         `;
 
       GraphWrapper.insertAdjacentHTML("beforeend", result);
-      GraphBar = document.querySelectorAll("#graphbar");
+      GraphBar = document.querySelectorAll(".graphbox__bar");
     });
     
     GraphBar.forEach((el) => {
